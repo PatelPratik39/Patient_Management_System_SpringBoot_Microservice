451d4c972531eb56c16f2babe9ebc8f18582e479
PatelPratik39 <ptl.pratik717@gmail.com>
2025-03-31 22:15:31 -0500

    Initial Patient microservice with h2-database

---
+++ pateint-service/src/main/java/com/pm/pateintservice/model/Patient.java
@@ -0,0 +1,39 @@
+package com.pm.pateintservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registerDate;
+}
